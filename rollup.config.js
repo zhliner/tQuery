48d86683f5eb65b4d21c93903c2c7885c95e825c
zhliner <zhliner@gmail.com>
2021-12-24 18:50:13 +0800

    直接构建到用户目录，便于调试跟踪。

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { terser } from 'rollup-plugin-terser'
 export default {
     input: 'tquery.js',
     output: {
-        file: 'release/tquery.min.js',
+        file: '../articlejs/base/tquery/tquery.min.js',
         format: 'iife',
         name: '$',
         banner: '/*! tQuery v0.5.3 | (c) zhliner@gmail.com 2021.10.20 | MIT License */',
